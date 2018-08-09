0e864712e896d4c8e7ec9c479ab27372b89e1b46
i066088 <i066088.b1.sap@gmail.com>
2018-08-09 20:52:26 +0800

    Update httpd-2.4.34.sh

--- httpd-2.4.34.sh
+++ httpd-2.4.34.sh
@@ -28,3 +28,27 @@ cd ../httpd-2.4.34 && \
 	./configure --with-apr=/usr/local/apr --enable-dialup --enable-reflector --enable-usertrack --enable-mime-magic --enable-log-forensic --enable-heartbeat --enable-heartmonitor --enable-dav-lock --enable-asis --enable-slotmem-plain --enable-proxy-fdpass --enable-data --enable-charset-lite --enable-echo --enable-watchdog --enable-mpms-shared="all" && \
 	make && make install
 	
+#the following is for packaging
+
+mkdir -p /home/apache_build/output/apache-httpd-2.4.34
+cd /home/apache_build/output/apache-httpd-2.4.34
+
+cp -r /usr/local/apache2/* ./
+
+cp /usr/local/apr/bin/apr-1-config ./bin/
+cp /usr/local/apr/bin/apu-1-config ./bin/
+
+mkdir -p ./lib/pkgconfig
+cp /usr/local/apr/lib/libapr-1.so.0 ./lib/
+cp /usr/local/apr/lib/libaprutil-1.so.0 ./lib/
+cp /usr/lib64/libexpat.so ./lib/libexpat.so.0
+cp /usr/lib64/libpcre.so.0 ./lib
+
+cp /usr/local/apr/lib/pkgconfig/apr-util-1.pc  ./lib/pkgconfig
+cp /usr/local/apr/lib/pkgconfig/apr-1.pc ./lib/pkgconfig
+cp /usr/lib64/pkgconfig/libpcre.pc ./lib/pkgconfig
+
+rm -r manual man include conf build
+
+cd ..
+tar czvf apache-httpd-2.4.34.tar.gz apache-httpd-2.4.34
